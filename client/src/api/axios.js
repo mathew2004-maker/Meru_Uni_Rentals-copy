a7558b39271b9b059baa9fd3cd57c63ae26c0e44
mathew2004-maker <mathewloyford@gmail.com>
2026-08-03 16:44:48 +0300

    Update axios API base URL for production

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    'https://meru-uni-rentals-api.onrender.com',
+    'https://meru-uni-rentals-api.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
