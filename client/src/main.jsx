66b9ef0fffdcddb3f71f6395f90443c8b9c8da88
mathew2004-maker <mathewloyford@gmail.com>
2026-08-03 16:23:36 +0300

    Update Google OAuth configuration

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,7 +9,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
-      <GoogleOAuthProvider clientId="1018654422461-48aa610ll17a0lbuh8etnioddlur29e3.apps.googleusercontent.com">
+      <GoogleOAuthProvider clientId="1018654422461-73k8hbkaa6d5v7atdj1vke9ksv48s0c1.apps.googleusercontent.com">
         <AuthProvider>
           <App />
         </AuthProvider>
