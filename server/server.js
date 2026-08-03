5de566d5a492ab5a5a61603f55a37fd891281e84
mathew2004-maker <mathewloyford@gmail.com>
2026-08-03 18:31:19 +0300

    changed cors

--- server/server.js
+++ server/server.js
@@ -10,19 +10,19 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// app.use(
-//   cors({
-//     origin: [
-//       'http://localhost:3000',
-//       'http://localhost:5173',
-//       'https://meru-uni-rentals-copy.vercel.app',
-//     ],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:5173',
+      'https://meru-uni-rentals-copy.vercel.app',
+    ],
+    credentials: true,
+  })
+);
 
 
-app.use(cors());
+
 app.use(express.json());
 
 
