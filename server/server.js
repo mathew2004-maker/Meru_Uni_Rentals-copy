c7e58764fd7d30964eb04c0182d4b68e9c6c5d66
mathew2004-maker <mathewloyford@gmail.com>
2026-08-03 18:34:24 +0300

    cors again boy

--- server/server.js
+++ server/server.js
@@ -10,19 +10,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:5173',
-      'https://meru-uni-rentals-copy.vercel.app',
-    ],
-    credentials: true,
-  })
-);
-
-
-
+app.use(cors());
 app.use(express.json());
 
 
